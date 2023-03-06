d154b9c8b1ff5f98f6165e2c059d702193940705
Семен <marbledrop@yandex.ru>
2023-03-06 22:08:37 +0300

    Add Exercise 23_1

---
+++ Exercise_23_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Программа для нахождения кубов чисел от единицы до заданного");
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out int x);
+
+double Func(int x)
+{
+    return Math.Pow(x, 3);
+}
+
+int i = 1;
+while (i <= x)
+{
+    Console.Write(Func(i));
+    if (i < x)
+    Console.Write(", ");
+    i++;
+}
